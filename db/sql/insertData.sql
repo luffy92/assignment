b6415f00cc4cfa606f8db91e5b48755723815883
HS Teh <luffy-92@hotmail.com>
2022-07-30 02:01:23 +0800

    Setup table and sample data

---
+++ db/sql/insertData.sql
@@ -0,0 +1,10 @@
+INSERT INTO todoitem (name, description,completed) 
+VALUES ('Task 1', 'Design',true);
+INSERT INTO todoitem (name, description,completed) 
+VALUES ('Task 2', 'Develop',true);
+INSERT INTO todoitem (name, description,completed)
+VALUES ('Task 3', 'Testing',false);
+INSERT INTO todoitem (name, description,completed)
+VALUES ('Task 4', 'Debug',false);
+INSERT INTO todoitem (name, description,completed)
+VALUES ('Task 5', 'Deploy',false);
